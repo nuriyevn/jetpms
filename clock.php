0b7a05af3a18809e10854029b4e71022598fac74
root <nuriyevn@gmail.com>
2015-11-12 20:57:16 +0000

    clock and  timers in  js

---
+++ clock.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Clock sample in JavaScript</title>
+<script src="./clock2.js"> 
+</script>
+</head>
+
+<body onload=display_ct();>
+<span id='ct' ></span>
+
+</body>
+</html>
