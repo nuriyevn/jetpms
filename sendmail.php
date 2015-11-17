f4bb76a8e8a7f0a4b8c9cb817daa25f77ce45889
nuriyevn <nuriyevn@gmail.com>
2015-11-17 17:49:02 +0200

    Sendmail minor modification

--- sendmail.php
+++ sendmail.php
@@ -3,13 +3,13 @@
 <title>Email sender program on PHP</title>
 </head>
 <body>
-
+<form action="./sendmail.php" method="post">
 <label>From:</label>
 <label for="from_">customer-care@jetpms.com</label>
 <br>
 <br>
 <label>To:</label>
-<input type="text" name="to_" value="">
+<input type="text" name="to_" value="mamahostel.od@gmail.com">
 <br>
 <br>
 <input type="text" name="subject_box" value="Some subject">
@@ -20,17 +20,28 @@ Put some text here...
 </textarea>
 <br>
 
-<input type="button" name="sendbutton" value="Send!">
+<input type="submit" name="sendbutton" value="Send!">
 <br>
 <br>
+
+</form>
 
 
 <?php
-$message = "Line1\rLine2\rLine3";
-$message = wordwrap($message, 70, "\r\n");
 
-mail("nuriyevn@gmail.com", "Some subject", $message);
+if (isset($_POST['sendbutton']))
+{
 
+	$subject = $_POST['subject_box'];
+	$to = $_POST['to_'];
+
+	var_dump($subject);
+
+	$message = "Line1\rLine2\rLine3";
+	$message = wordwrap($message, 70, "\r\n");
+
+	mail($to, $subject, $message);
+}
 ?>
 
 Please check spam if you can't get an email.
