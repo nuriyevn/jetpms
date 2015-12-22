8eb59c014892784da10ee7fd986f7a1e055f7007
nuriyevn <nuriyevn@gmail.com>
2015-12-22 20:57:53 +0200

    Absolute pathes in php must be prepended with ABSPATH definition

---
+++ app-config.php
@@ -0,0 +1,8 @@
+<?php
+// in php scripts all pathes should be in such manner
+// example  https//jetpms.com/img/loading.gif
+// $path = ABSPATH."/img/loading.gif"
+// pretty easy, huh?
+
+if ( !defined('ABSPATH') )
+    define('ABSPATH', dirname(__FILE__) . '/');
