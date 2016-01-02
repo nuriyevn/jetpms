5b3794daf758a8d9dba200f1373d4d8f1402e31f
umkin <naum.oleks@gmail.com>
2016-01-02 14:38:34 +0200

    task https://jet.kanbantool.com/b/195250-jet#7599454 complete

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
         <div class="row">
             <div class="col-md-6 col-md-offset-4">
                 <ul class="nav nav-tabs">
-                    <li><a href="index.php"><i class="fa fa-home"></i>  Main</span></a></li>
+                    <li><a href="index.php"><i class="fa fa-home"></i> Main</span></a></li>
                     <li><a href="#product"><i class="fa fa-cog fa-spin"></i> How it works</a></li>
                     <li><a href="#price"><i class="fa fa-usd"></i> Pricing</a></li>
                     <li><a href="#contacts"><i class="fa fa-map-marker"></i> Contact us</a></li>
@@ -44,27 +44,32 @@
 
             <div class="col-md-5 col-md-offset-1">
                 <ul class="lead">
-                    <li class="fa fa-hand-o-right"> Полная автоматизация работы хостела. Автоматическое
-                        выставление счетов и платежей. Отчеты
+                    <li class="fa fa-hand-o-right"> Удобный календарь заказов и номеров. Вся работа - как на ладони.
                     </li>
-                    <li class="fa fa-hand-o-right"> 2 way синхронизация  booking.com,
+                    <li class="fa fa-hand-o-right"> Менеждер каналов с 2 way синхронизацией booking.com,
                         hostelworld.com, ostrovok.ru
                     </li>
-                    <li class="fa fa-hand-o-right"> Документооборот адаптирован для Украины</li>
+                    <li class="fa fa-hand-o-right"> Учет денег, материалов, труда сотрудников. Просто и понятно.</li>
+                    <Li class="fa fa-hand-o-right"> Гибкая и легкая настройка, справится даже школьник.</Li>
                 </ul>
             </div>
             <div class="col-md-5">
                 <ul class="lead">
-                    <li class="fa fa-hand-o-right"> Работа с ПК и мобильными устройствами</li>
-                    <li class="fa fa-hand-o-right"> Не требует покупки дополнительного оборудования</li>
-                    <li class="fa fa-hand-o-right"> 120 дней БЕСПЛАТНОГО пользования</li>
+                    <li class="fa fa-hand-o-right"> Почта Gmail. Полная итеграция, переписка с гостями прямо в
+                        программе
+                    </li>
+                    <li class="fa fa-hand-o-right"> Вы все сделаете. Напоминания о необходимых действиях. Создание
+                        списков дел.
+                    </li>
+                    <li class="fa fa-hand-o-right"> Автоматизация всех рутинных действий.</li>
+                    <li class="fa fa-hand-o-right"> 101 дней БЕСПЛАТНОГО пользования</li>
                 </ul>
             </div>
         </div>
         <div class="row">
             <p>
                 <a href="/signup/index.php">
-                    <button type="button" class="btn btn-danger btn-lg">Попробуйте бесплатно</button>
+                    <button type="button" class="btn btn-danger btn-lg">Попробуйте бесплатно 101 день</button>
                 </a>
                 <!--                <a href="#">-->
                 <!--                    <button type="button" class="btn btn-success btn-lg">Демо версия</button>-->
@@ -80,116 +85,182 @@
 
 <!--SECOND BLOCK-->
 <div class="container" id="product">
-    <div class="row">
-        <h2 class="line-height">Cистема позволяет легко продавать номера и автоматизировать управление хостелом</h2>
+    <div class="row text-center">
+        <h2 class="line-height">Cистема легко продает номера и автоматизирует управление хостелом</h2>
 
         <div class="col-md-4">
-            <div class="row text-center">
-                <img src="img/jet_logo.png" alt="jet_logo">
+            <div class="row">
+                <img src="img/jet_blue.png" alt="jet_logo">
             </div>
             <a href="#">
-                <h2>Jet PMS</h2>
-            </a><br>система для автоматизации хостела
+                <h2 class="">Calendar PMS</h2>
+            </a>визуализация всех заказов по датам и номерам
             <ul>
-                <li  class="fa fa-arrow-right">Выставляйте на продажу весь номерной фонд в системах
-                    online-бронирования и на рецепции в одной программе
+                <li class="fa fa-arrow-right"> Красочный наглядный интерфейс сразу расскажет о положении в хостеле
                 </li>
-                <li  class="fa fa-arrow-right">Управляйте всеми операциями в хостеле, используя одно
-                    приложение
+                <li class="fa fa-arrow-right"> Управляйте всеми операциями с заказами в один клик, используя одно
+                    окно
                 </li>
-                <li  class="fa fa-arrow-right">Выставляйте счета, платежи, отправляйте подтверждения о
-                    бронировании
+                <li class="fa fa-arrow-right"> Система выставлет счета и отправляет подтверждения о
+                    бронировании автоматически
                 </li>
-                <li  class="fa fa-arrow-right">Формируйте отчеты, планируйте продажи номеров</li>
+                <li class="fa fa-arrow-right"> Настриивайте отображение под свой вкус</li>
             </ul>
+            <a href="#">
+                <buttom class="btn btn-primary btn-md features-btn">подробнее</buttom>
+            </a>
         </div>
 
         <div class="col-md-4">
             <div class="row text-center">
-                <img src="img/Chanel_manager_logo.png" alt="Chanel_manager_logo.png">
+                <img src="img/jet_orange.png" alt="Chanel_manager_logo.png">
             </div>
             <a href="#">
                 <h2>Channel Manager</h2>
-            </a><br>инструмент управления online продажами
+            </a>инструмент управления online продажами
             <ul>
+                <li class="fa fa-arrow-right"> Выставляйте на продажу весь номерной фонд в системах
+                    online-бронирования и на рецепции в одной программе
+                </li>
                 <li class="fa fa-arrow-right"> Обновляйте информацию об хостеле во всех системах бронирования
                     одновременно
                 </li>
-                <li  class="fa fa-arrow-right"> Редактируйте стоимость и наличие номерного фонда из одного
+                <li class="fa fa-arrow-right"> Редактируйте стоимость и наличие номерного фонда из одного
                     интерфейса
                 </li>
-                <li class="fa fa-arrow-right"> Экономьте свое время на работу с ОТА-системами</li>
-                <li class="fa fa-arrow-right"> При отмене бронирования, номер автоматически выставляется на
-                    продажу
-                </li>
             </ul>
+            <a href="#">
+                <buttom class="btn btn-primary btn-md">подробнее</buttom>
+            </a>
         </div>
 
         <div class="col-md-4">
             <div class="row text-center">
-                <img src="img/Booking_buttom_logo.png" alt="OBooking_buttom_logo">
+                <img src="img/jet_green.png" alt="OBooking_buttom_logo">
             </div>
             <a href="#">
-                <h2>Booking Button</h2></a><br>модуль online бронирования на сайте отеля
+                <h2>Booking Button</h2></a>
+            модуль online бронирования на сайте хостела
             <ul>
-                <li  class="fa fa-arrow-right"> Продавайте номера напрямую через сайт – удобно и без
+                <li class="fa fa-arrow-right"> Продавайте номера напрямую через сайт – удобно и без
                     посредников
                 </li>
-                <li  class="fa fa-arrow-right"> Позволяйте вашим гостям осуществлять бронь всего в пару
+                <li class="fa fa-arrow-right"> Позволяйте вашим гостям осуществлять бронь всего в пару
                     кликов
                 </li>
-                <li  class="fa fa-arrow-right"> Отслеживайте посетителей сайта и улучшайте конверсию</li>
                 <li class="fa fa-arrow-right"> При бронировании номер автоматически снимается с продажи на
                     всех порталах
                 </li>
             </ul>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-4">
-            <buttom class="btn btn-primary btn-md">подробнее</buttom>
-        </div>
-        <div class="col-md-4">
-            <buttom class="btn btn-primary btn-md">подробнее</buttom>
-        </div>
-        <div class="col-md-4">
             <buttom class="btn btn-primary btn-md">подробнее</buttom>
         </div>
     </div>
+
     <br><br>
 
-    <div class="row">
-        <div class="col-md-2 col-md-offset-1">
-            <img src="img/online-manager.png" alt="online-manager_logo">
-
-            <p>Online управление отелем</p>
+    <div class="row text-center">
+        <div class="col-md-4">
+            <div class="row">
+                <img src="img/jet_blue.png" alt="jet_logo">
+            </div>
+            <a href="#">
+                <h2 class="">Accounting PMS</h2>
+            </a>
+            учет ресурсов и статистика
+            <ul>
+                <li class="fa fa-arrow-right"> Контроль за движением наличных и безналичных денег
+                </li>
+                <li class="fa fa-arrow-right"> Учет трудозатрат, начисление заработных плат
+                </li>
+                <li class="fa fa-arrow-right"> Аналитическая статистика покажет слабые и сильные места
+                </li>
+                <li class="fa fa-arrow-right"> Учет использования расходников, статистика расходов
+                </li>
+            </ul>
+            <a href="#">
+                <buttom class="btn btn-primary btn-md features-btn">подробнее</buttom>
+            </a>
         </div>
-        <div class="col-md-2">
-            <img src="img/bron.png" alt="bron_logo">
 
-            <p>Бронирование номера с вашего сайта</p>
+        <div class="col-md-4">
+            <div class="row text-center">
+                <img src="img/jet_orange.png" alt="Chanel_manager_logo.png">
+            </div>
+            <a href="#">
+                <h2>Gmail integration</h2>
+            </a>
+            Вся рабочая переписака в одном месте
+            <ul>
+                <li class="fa fa-arrow-right"> Выставляйте на продажу весь номерной фонд в системах
+                    online-бронирования и на рецепции в одной программе
+                </li>
+                <li class="fa fa-arrow-right"> Обновляйте информацию об хостеле во всех системах бронирования
+                    одновременно
+                </li>
+                <li class="fa fa-arrow-right"> Редактируйте стоимость и наличие номерного фонда из одного
+                    интерфейса
+                </li>
+            </ul>
+            <a href="#">
+                <buttom class="btn btn-primary btn-md">подробнее</buttom>
+            </a>
         </div>
-        <div class="col-md-2"><img src="img/2way.png" alt="2way_logo">
 
-            <p>2way синхронизация с системами онлайн бронирования</p>
+        <div class="col-md-4">
+            <div class="row text-center">
+                <img src="img/jet_green.png" alt="OBooking_buttom_logo">
+            </div>
+            <a href="#">
+                <h2>Todoist</h2></a>
+            Списки дел и напоминания
+            <ul>
+                <li class="fa fa-arrow-right"> Вы никогда не забудете принять оплату и "выселить" гостя
+                </li>
+                <li class="fa fa-arrow-right"> Напоминания "приянять" предоплату
+                </li>
+                <li class="fa fa-arrow-right"> Создание собственных списков напоминий и дел
+                </li>
+                <li class="fa fa-arrow-right"> Назначение дел сотрудникам
+                </li>
+            </ul>
+            <buttom class="btn btn-primary btn-md">подробнее</buttom>
         </div>
-        <div class="col-md-2">
-            <img src="img/save.png" alt="save_logo">
-
-            <p>Надежность хранения данных</p>
-        </div>
-        <div class="col-md-2">
-            <img src="img/help.png" alt="help_logo">
-
-            <p>Техническая поддержка 24/7</p>
-        </div>
-        <div class="col-md-1"></div>
     </div>
+
+    <br><br>
+
+<!--    <div class="row">-->
+<!--        <div class="col-md-2 col-md-offset-1">-->
+<!--            <img src="img/online-manager.png" alt="online-manager_logo">-->
+<!---->
+<!--            <p>Online управление отелем</p>-->
+<!--        </div>-->
+<!--        <div class="col-md-2">-->
+<!--            <img src="img/bron.png" alt="bron_logo">-->
+<!---->
+<!--            <p>Бронирование номера с вашего сайта</p>-->
+<!--        </div>-->
+<!--        <div class="col-md-2"><img src="img/2way.png" alt="2way_logo">-->
+<!---->
+<!--            <p>2way синхронизация с системами онлайн бронирования</p>-->
+<!--        </div>-->
+<!--        <div class="col-md-2">-->
+<!--            <img src="img/save.png" alt="save_logo">-->
+<!---->
+<!--            <p>Надежность хранения данных</p>-->
+<!--        </div>-->
+<!--        <div class="col-md-2">-->
+<!--            <img src="img/help.png" alt="help_logo">-->
+<!---->
+<!--            <p>Техническая поддержка 24/7</p>-->
+<!--        </div>-->
+<!--        <div class="col-md-1"></div>-->
+<!--    </div>-->
     <div class="row">
         <img src="img/baner.png" alt="baner" width="100%">
     </div>
-
-    <h2 class="line-height">Преимущества работы с Jet PMS</h2>
+    <br><br>
+    <h2 class="line-height text-center">Преимущества работы с Jet PMS</h2>
 
     <div class="row">
         <div class="col-md-4">
@@ -298,7 +369,7 @@
 
         <div class="row">
             <p><a href="/signup/index.php">
-                    <button type="button" class="btn btn-danger btn-lg">Попробуйте бесплатно 120 дней</button>
+                    <button type="button" class="btn btn-danger btn-lg">Попробуйте бесплатно 101 день</button>
                 </a>
                 <!--                <a href="#">-->
                 <!--                    <button type="button" class="btn btn-success btn-lg">Демо версия</button>-->
@@ -398,7 +469,7 @@
                 <br>Вы находитесь в надежных руках опытных специалистов</p>
 
             <p><a href="/signup/index.php">
-                    <button type="button" class="btn btn-danger btn-lg">Попробуйте бесплатно</button>
+                    <button type="button" class="btn btn-danger btn-lg">Попробуйте бесплатно 101 день</button>
                 </a>
                 <!--                <a href="#">-->
                 <!--                    <button type="button" class="btn btn-success btn-lg">Демо версия</button>-->
