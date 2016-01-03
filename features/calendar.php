cbfbdf7781845457dbc1d4d2179520fcf94db612
umkin <naum.oleks@gmail.com>
2016-01-03 02:09:33 +0200

    base calendar.php done

---
+++ features/calendar.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Jet PMS | Speed up your business</title>
+
+    <!-- Bootstrap -->
+    <link href="/css/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/bootstrap/bootstrap-theme.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+    <!-- Bootstrap customizable -->
+    <link href="/css/index.css" rel="stylesheet">
+    <link rel="shortcut icon" href="/favicon.png" type="image/x-icon">
+
+</head>
+<body>
+
+<!--FIRST BLOCK-->
+<div class="blue_block">
+    <div class="container">
+        <!--        nav& && login && signup  bar here -->
+        <div class="row">
+            <div class="col-md-6 col-md-offset-4">
+                <ul class="nav nav-tabs">
+                    <li><a href="../index.php#calendar"><i class="fa fa-home"></i> Main</span></a></li>
+                    <li><a href="#product"><i class="fa fa-cog fa-spin"></i> How it works</a></li>
+                    <li><a href="#price"><i class="fa fa-usd"></i> Pricing</a></li>
+                    <li><a href="#contacts"><i class="fa fa-map-marker"></i> Contact us</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2">
+                <a href="login/index.php" class="btn btn-success btn-lg" role="button">LOGIN TO APP</a>
+            </div>
+        </div>
+    </div>
+    <!--            end of na bar-->
+</div>
+<div class="container text-center" id="product">
+    <h1>Calendar</h1>
+
+    <h2>Наглядное визуальное оформление</h2>
+
+    <img src="/img/calendar.png" alt="calendar">
+
+    <div class="row text-left">
+        <div class="col-md-5 col-md-offset-1">
+            <ul>
+                <li class="fa fa-arrow-right"> индикация групповых бронирований</li>
+                <br>
+                <li class="fa fa-arrow-right"> обозначение ранних или поздних заездов</li>
+                <br>
+                <li class="fa fa-arrow-right"> цветовое обозначение статуса заказа</li>
+                <br>
+            </ul>
+        </div>
+        <div class="col-md-5">
+            <ul>
+                <li class="fa fa-arrow-right"> указание задолженности гостя</li>
+                <br>
+                <li class="fa fa-arrow-right"> иконки специальных состояний</li>
+                <br>
+                <li class="fa fa-arrow-right"> строка остатков по категориям</li>
+                <br>
+            </ul>
+        </div>
+        <br><br><br><br><br><br><br>
+        <h2 class="text-center">Простое и удобное использование</h2>
+
+        <img src="/img/calendar.png" alt="calendar">
+
+        <div class="row text-left">
+            <div class="col-md-5 col-md-offset-1">
+                <ul>
+                    <li class="fa fa-arrow-right"> дополнительная информация о заказе при наведении курсора</li>
+                    <br>
+                    <li class="fa fa-arrow-right"> все основные операции в два клика</li>
+                    <br>
+                    <li class="fa fa-arrow-right"> система контролирует, у вас не будет шанса сделать ошибку</li>
+                    <br>
+                </ul>
+            </div>
+            <div class="col-md-5">
+                <ul>
+                    <li class="fa fa-arrow-right"> настройка шахматки путем сворачивания или перетаскивания элементов</li>
+                    <br>
+                    <li class="fa fa-arrow-right"> управление тарифами из шахматки</li>
+                    <br>
+                    <li class="fa fa-arrow-right"> дополнительные опцие через клик правой кнопки мыши</li>
+                    <br>
+                </ul>
+            </div>
+    </div>
+</div>
+</div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="http://code.jquery.com/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="/js/bootstrap/bootstrap.min.js"></script>
+<script src="/js/index.js"></script>
+</body>
